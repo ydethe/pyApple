2def629659f41c96eba37d5ec881b2c408a39be2
ydethe <498517+ydethe@users.noreply.github.com>
2019-03-16 17:42:18 +0100

    Amelioration tests

--- pyApple/tests/Testsimple.py
+++ pyApple/tests/Testsimple.py
@@ -1,20 +1,20 @@
 # -*- coding: latin-1 -*-
 
 #     This file is part of pyApple.
-# 
+#
 #     pyApple is free software: you can redistribute it and/or modify
 #     it under the terms of the Lesser GNU General Public License as published by
 #     the Free Software Foundation, either version 3 of the License, or
 #     (at your option) any later version.
-# 
+#
 #     pyApple is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     Lesser GNU General Public License for more details.
-# 
+#
 #     You should have received a copy of the Lesser GNU General Public License
 #     along with pyApple.  If not, see <http://www.gnu.org/licenses/>.
-
+import unittest
 import sys
 
 # Put the path where pyApple is located here
@@ -27,20 +27,25 @@ from simple_reader import *
 from gui_elem import *
 
 
-d = Simple_Reader('data.txt')
+class TestSimple (unittest.TestCase):
+    def test(self):
+        d = Simple_Reader('data.txt')
 
-carte = Map("Trace au sol", True, projection='merc')
-tr_sol = Line("Trace au sol", d['lon'], d['lat'], color='b')
-carte.addLine(tr_sol)
+        carte = Map("Trace au sol", True, projection='merc')
+        tr_sol = Line("Trace au sol", d['lon'], d['lat'], color='b')
+        carte.addLine(tr_sol)
 
-rep = Axe("Altitude")
-alt = Line(u"Altitude", d['t'], d['alt'], color='b')
-rep.addLine(alt)
+        rep = Axe("Altitude")
+        alt = Line(u"Altitude", d['t'], d['alt'], color='b')
+        rep.addLine(alt)
 
-fen = AppleWindow()
-pl = fen.createBoard(u"Satellite")
-pl.addAxe(rep)
-pl.addAxe(carte)
+        fen = AppleWindow()
+        pl = fen.createBoard(u"Satellite")
+        pl.addAxe(rep)
+        pl.addAxe(carte)
 
-fen.display()
+        fen.display()
 
+
+if __name__ == '__main__':
+    unittest.main()
